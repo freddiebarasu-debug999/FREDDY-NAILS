5e3851f5317817a9b6f865f2b2f00ef6854d7f75
freddiebarasu-debug999 <freddiebarasu@gmail.com>
2026-08-22 19:34:24 +0200

    Add Gallery, Booking, Offers, Reviews, FAQ, and Contact sections

--- app/page.js
+++ app/page.js
@@ -14,87 +14,75 @@ import Footer from "@/components/Footer";
 import ChatBot from "./ChatBot";
 import Reveal from "@/components/Reveal";
 import WelcomePopup from "@/components/WelcomePopup";
-
 export default function Home() {
   return (
     <>
       <WelcomePopup />
       <OfferTab />
       <Header />
-
       <Hero />
-
       <div className="h-px max-w-[1180px] mx-auto bg-line" />
-
-      <div className="py-10 md:py-16">
-        <Reveal>
-          <Services />
-        </Reveal>
-      </div>
-
-      <div className="py-10 md:py-16">
-        <Reveal delay={100}>
-          <ShapeGuide />
-        </Reveal>
-      </div>
-
+      {/* Gallery */}
       <div className="py-10 md:py-16">
         <Reveal>
           <Gallery />
         </Reveal>
       </div>
-
       <div className="h-px max-w-[1180px] mx-auto bg-line" />
-
+      {/* Services & Pricing */}
+      <div className="py-10 md:py-16">
+        <Reveal>
+          <Services />
+        </Reveal>
+      </div>
+      <div className="py-10 md:py-16">
+        <Reveal delay={100}>
+          <ShapeGuide />
+        </Reveal>
+      </div>
+      <div className="h-px max-w-[1180px] mx-auto bg-line" />
+      {/* Booking */}
       <div className="py-10 md:py-16">
         <Reveal>
           <Booking />
         </Reveal>
       </div>
-
       <div className="h-px max-w-[1180px] mx-auto bg-line" />
-
+      {/* Offers */}
       <div className="py-10 md:py-16">
         <Reveal>
           <Offers />
         </Reveal>
       </div>
-
       <div className="h-px max-w-[1180px] mx-auto bg-line" />
-
+      {/* Reviews */}
       <div className="py-10 md:py-16">
         <Reveal>
           <Reviews />
         </Reveal>
       </div>
-
       <div className="h-px max-w-[1180px] mx-auto bg-line" />
-
+      {/* FAQ */}
       <div className="py-10 md:py-16">
         <Reveal>
           <FAQ />
         </Reveal>
       </div>
-
       <div className="h-px max-w-[1180px] mx-auto bg-line" />
-
       {/* About the Studio */}
       <div className="py-10 md:py-16">
         <Reveal>
           <About />
         </Reveal>
       </div>
-
       <div className="h-px max-w-[1180px] mx-auto bg-line" />
-
+      {/* Contact */}
       <div className="py-10 md:py-16">
         <Reveal>
           <Contact />
         </Reveal>
       </div>
-
       <Footer />
-
       <ChatBot />
     </>
   );
