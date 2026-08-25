75aa6a15dcc39d4b89a20fe3a2d59bf21d57956d
freddiebarasu-debug999 <freddiebarasu@gmail.com>
2026-08-25 23:23:42 +0200

    Create OffersPage component with layout structure

---
+++ app/offers/page.js
@@ -0,0 +1,21 @@
+import Header from "@/components/Header";
+import Offers from "@/components/Offers";
+import Footer from "@/components/Footer";
+import OfferTab from "@/components/OfferTab";
+import ChatBot from "../ChatBot";
+
+export default function OffersPage() {
+  return (
+    <>
+      <OfferTab />
+      <Header />
+
+      <main className="min-h-screen bg-[#11100f] text-[#f4eee6]">
+        <Offers />
+      </main>
+
+      <Footer />
+      <ChatBot />
+    </>
+  );
+}
